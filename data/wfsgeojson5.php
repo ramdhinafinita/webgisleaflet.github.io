860a146bade881b58d96ffab4bbb1327228b8e61
ramdhinafinita <ramdhinafinita@mail.ugm.ac.id>
2022-07-28 13:17:42 +0700

    first

---
+++ data/wfsgeojson5.php
@@ -0,0 +1,9 @@
+<?php
+	// $wfsUrl = file_get_contents("http://geoportal.slemankab.go.id/geoserver/geonode/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=geonode:analisis_resiko_bencana_gunung_api&outputFormat=json");
+
+	$wfsUrl = file_get_contents("http://localhost:8081/geoserver/SLEMAN_COV19/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=SLEMAN_COV19%3AADMIN_LN_ALL_SLEMAN_Proj&outputFormat=application%2Fjson");
+
+	header('Content-type: application/json');
+	echo ($wfsUrl);
+	# Jika terdapat &maxFeatures=50 pada url wfs geojson, dihapus supaya jumlah feature tidak dibatasi
+?>
